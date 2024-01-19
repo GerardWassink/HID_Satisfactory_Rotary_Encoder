c7db4c00e0f0e15199ec10fb2b3276bb95520c62
GerardWassink <GerardWassink@users.noreply.github.com>
2024-01-19 09:28:45 +0100

    Update HID_Satisfactory_Rotary_Encoder.ino
    Date correction

--- HID_Satisfactory_Rotary_Encoder.ino
+++ HID_Satisfactory_Rotary_Encoder.ino
@@ -32,7 +32,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  * ------------------------------------------------------------------------- *
- *       Copyright (C) November 2021 Gerard Wassink
+ *       Copyright (C) Januari 2024 Gerard Wassink
  * ------------------------------------------------------------------------- */
 
 
